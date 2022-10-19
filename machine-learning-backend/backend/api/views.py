a5fba95db3bc40d7159b474b0f13b2084c43d816
phuong1331phuong <phuong1331phuong@gmail.com>
2022-10-19 17:01:48 +0700

    change virtual of back end

--- machine-learning-backend/backend/api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
-
-# Create your views here.
-
-
-def index(request):
-    return HttpResponse("<h1>Hello World!</h1>")
--- machine-learning-backend/backend/wordrecognition/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models.image_dto import ImageDto
-
-admin.site.register(ImageDto)
